cc363d9daaabf6d7024cc65cd2d8e02b7e9de139
MoonLGH <MoonLMC1212@gmail.com>
2021-06-25 00:23:41 +0700

    change 20 bot to 10 to reduce memory usage

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ let botexist = false
 function makebot(ip, ver, nick,msg) {
     if(botexist === true) return msg.channel.send("Bot is already running")
     const chatbot = 0
-    const bots = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    const bots = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     bot = mineflayer.createBot({
         host: ip,
